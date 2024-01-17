1e298cc3b9425380667bafb1d31fdcc67870b408
wooandrich <brad1182@naver.com>
2024-01-17 16:10:16 +0900

    feat : InfoTalk 도메인 입력

---
+++ src/main/java/homeat/backend/domain/post/InfoTalk.java
@@ -0,0 +1,16 @@
+package homeat.backend.domain.post;
+
+import javax.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InfoTalk {
+}

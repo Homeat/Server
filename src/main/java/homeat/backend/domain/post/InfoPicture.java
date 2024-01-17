4a06a39718d0ca2e1444d5289c819acf9df4e3cb
wooandrich <brad1182@naver.com>
2024-01-17 16:15:28 +0900

    feat : InfoPicture 도메인 입력

---
+++ src/main/java/homeat/backend/domain/post/InfoPicture.java
@@ -0,0 +1,36 @@
+package homeat.backend.domain.post;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InfoPicture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "infoPicture_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "infotalk_id")
+    private InfoTalk infoTalk;
+
+    private String url;
+}

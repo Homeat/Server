5699bbf2e06974cd2e089e022c90956760fdf751
wooandrich <brad1182@naver.com>
2024-01-17 16:12:57 +0900

    feat : InfoTalkComment 도메인 입력

---
+++ src/main/java/homeat/backend/domain/post/InfoTalkComment.java
@@ -0,0 +1,38 @@
+package homeat.backend.domain.post;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InfoTalkComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "infotalk_comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "infotalk_id")
+    private InfoTalk infoTalk;
+
+    /*@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;*/
+
+    private String content;
+}

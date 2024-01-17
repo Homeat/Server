99419d5f3893fc01d39611fdedbb51dd0825c777
wooandrich <brad1182@naver.com>
2024-01-17 16:14:11 +0900

    feat : InfoTalk 도메인 수정

--- src/main/java/homeat/backend/domain/post/InfoTalk.java
+++ src/main/java/homeat/backend/domain/post/InfoTalk.java
@@ -1,6 +1,12 @@
 package homeat.backend.domain.post;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,4 +19,24 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InfoTalk {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "infotalk_id")
+    private Long id;
+
+    /*@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;*/
+
+    private String title;
+    private String content;
+
+
+    private Integer like;
+    private Integer view;
+    private Integer commentNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Save save;
 }

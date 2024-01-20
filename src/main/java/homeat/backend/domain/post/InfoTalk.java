3f790b86b5dac5d449f421398524026b972da1d5
wooandrich <brad1182@naver.com>
2024-01-20 19:23:38 +0900

    feat : member 연관관계 추가

--- src/main/java/homeat/backend/domain/post/InfoTalk.java
+++ src/main/java/homeat/backend/domain/post/InfoTalk.java
@@ -1,12 +1,16 @@
 package homeat.backend.domain.post;
 
+import homeat.backend.user.domain.Member;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,9 +29,9 @@ public class InfoTalk {
     @Column(name = "infotalk_id")
     private Long id;
 
-    /*@ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
-    private Member member;*/
+    private Member member;
 
     private String title;
     private String content;

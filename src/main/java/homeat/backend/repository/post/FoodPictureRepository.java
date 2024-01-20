dcb73dc2cd6f8008e65f78304a4be9f5a387b700
wooandrich <brad1182@naver.com>
2024-01-20 20:20:09 +0900

    chore : @Repository 수정

--- src/main/java/homeat/backend/repository/post/FoodPictureRepository.java
+++ src/main/java/homeat/backend/repository/post/FoodPictureRepository.java
@@ -2,6 +2,8 @@ package homeat.backend.repository.post;
 
 import homeat.backend.domain.post.FoodPicture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FoodPictureRepository extends JpaRepository<FoodPicture, Long> {
 }
--- src/main/java/homeat/backend/repository/post/FoodTalkRepository.java
+++ src/main/java/homeat/backend/repository/post/FoodTalkRepository.java
@@ -2,6 +2,8 @@ package homeat.backend.repository.post;
 
 import homeat.backend.domain.post.FoodTalk;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FoodTalkRepository extends JpaRepository<FoodTalk, Long> {
 }

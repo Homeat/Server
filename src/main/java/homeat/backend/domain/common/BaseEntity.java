3b7ffb6e0bd85b822bac0edd05ca7a72e491affb
wooandrich <brad1182@naver.com>
2024-01-20 21:15:28 +0900

    feat : 어노테이션 적용 및 BaseEntity 사용가능하게 변경

--- src/main/java/homeat/backend/domain/common/BaseEntity.java
+++ src/main/java/homeat/backend/domain/common/BaseEntity.java
@@ -1,10 +1,17 @@
 package homeat.backend.domain.common;
 
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @CreatedDate

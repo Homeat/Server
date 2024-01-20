79b3b8124fd7d549ee864b9a09838a936a57197e
wooandrich <brad1182@naver.com>
2024-01-20 20:20:09 +0900

    chore : @Repository 수정

--- src/main/java/homeat/backend/repository/post/InfoTalkCommentRepository.java
+++ src/main/java/homeat/backend/repository/post/InfoTalkCommentRepository.java
@@ -2,6 +2,8 @@ package homeat.backend.repository.post;
 
 import homeat.backend.domain.post.InfoTalkComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InfoTalkCommentRepository extends JpaRepository<InfoTalkComment, Long> {
 }

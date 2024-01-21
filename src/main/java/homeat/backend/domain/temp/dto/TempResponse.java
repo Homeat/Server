1a082f2ddd609ebabd49804c9edf83240af44c6f
헥스/박해승 <2001phs92@gmail.com>
2024-01-22 03:57:10 +0900

    Feat: temp의 dto 작성

---
+++ src/main/java/homeat/backend/domain/temp/dto/TempResponse.java
@@ -0,0 +1,17 @@
+package homeat.backend.domain.temp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class TempResponse {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TempTestDTO {
+        String testString;
+    }
+}

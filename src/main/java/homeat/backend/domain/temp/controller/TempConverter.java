868ccf0eb2cd2e40ea405933db885a292388da99
헥스/박해승 <2001phs92@gmail.com>
2024-01-22 04:03:24 +0900

    Feat: temp의 converter 작성

---
+++ src/main/java/homeat/backend/domain/temp/controller/TempConverter.java
@@ -0,0 +1,12 @@
+package homeat.backend.domain.temp.controller;
+
+import homeat.backend.domain.temp.dto.TempResponse;
+
+public class TempConverter {
+
+    public static TempResponse.TempTestDTO toTempTestDTO() {
+        return TempResponse.TempTestDTO.builder()
+                .testString("This is Test!")
+                .build();
+    }
+}

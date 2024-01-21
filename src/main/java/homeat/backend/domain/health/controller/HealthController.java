51d0f4220fa22266630e1737e297a947dcb88732
헥스/박해승 <2001phs92@gmail.com>
2024-01-22 04:14:32 +0900

    Feat: health check controller 작성

---
+++ src/main/java/homeat/backend/domain/health/controller/HealthController.java
@@ -0,0 +1,15 @@
+package homeat.backend.domain.health.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/health")
+public class HealthController {
+
+    @GetMapping("")
+    public String healthCheck() {
+        return "I'm healthy!";
+    }
+}

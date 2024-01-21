6d8aa44e2097cc2a5ceec2fb8da68a9339a4719f
헥스/박해승 <2001phs92@gmail.com>
2024-01-22 03:27:55 +0900

    Feat: ApiPayload 작성

---
+++ src/main/java/homeat/backend/global/payload/ApiPayload.java
@@ -0,0 +1,30 @@
+package homeat.backend.global.payload;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "data"})
+public class ApiPayload<T> {
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    // 성공
+    public static <T> ApiPayload<T> onSuccess(String code, String message, T data) {
+        return new ApiPayload<>(true, code, message, data);
+    }
+
+    // 실패
+    public static <T> ApiPayload<T> onFailure(String code, String message, T data) {
+        return new ApiPayload<>(false, code, message, data);
+    }
+}

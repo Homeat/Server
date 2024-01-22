bd0f122cedd5f9962792ff4ac1ea5938313bf8f3
헥스/박해승 <2001phs92@gmail.com>
2024-01-23 02:39:53 +0900

    Refactor: create user controller 수정

--- src/main/java/homeat/backend/domain/user/controller/MemberController.java
+++ src/main/java/homeat/backend/domain/user/controller/MemberController.java
@@ -2,14 +2,19 @@ package homeat.backend.domain.user.controller;
 
 import homeat.backend.domain.user.dto.MemberRequest;
 import homeat.backend.domain.user.dto.MemberResponse;
+import homeat.backend.domain.user.entity.Member;
+import homeat.backend.domain.user.service.MemberCommandService;
 import homeat.backend.global.payload.ApiPayload;
+import homeat.backend.global.payload.CommonSuccessStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/v1/members")
 public class MemberController {
 
@@ -17,6 +22,7 @@ public class MemberController {
 
     @PostMapping("/")
     public ApiPayload<MemberResponse.CreateResultDTO> create(@RequestBody @Valid MemberRequest.CreateDto request) {
-        return null;
+        Member member = memberCommandService.createMember(request);
+        return ApiPayload.onSuccess(CommonSuccessStatus.CREATED, MemberConverter.toCreateResultDTO(member));
     }
 }

f6024562de6ea8debbd70d447ec8729960a89738
wooandrich <brad1182@naver.com>
2024-01-24 20:28:17 +0900

    feat : 임시저장 api 추가

--- src/main/java/homeat/backend/domain/post/service/FoodTalkService.java
+++ src/main/java/homeat/backend/domain/post/service/FoodTalkService.java
@@ -2,6 +2,7 @@ package homeat.backend.domain.post.service;
 
 import homeat.backend.domain.post.entity.FoodTalk;
 import homeat.backend.domain.post.dto.FoodTalkDTO;
+import homeat.backend.domain.post.entity.Save;
 import homeat.backend.domain.post.repository.FoodTalkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,11 +23,25 @@ public class FoodTalkService {
                 .name(dto.getName())
                 .memo(dto.getMemo())
                 .tag(dto.getTag())
+                .save(Save.저장)
                 .build();
 
         foodTalkRepository.save(foodTalk);
 
-        return ResponseEntity.ok("음식토크 저장완료");
+        return ResponseEntity.ok("집밥토크 저장완료");
+    }
+    @Transactional
+    public ResponseEntity<?> tempSaveFoodTalk(FoodTalkDTO dto) {
+        FoodTalk foodTalk = FoodTalk.builder()
+                .name(dto.getName())
+                .memo(dto.getMemo())
+                .tag(dto.getTag())
+                .save(Save.임시저장)
+                .build();
+
+        foodTalkRepository.save(foodTalk);
+
+        return ResponseEntity.ok("집밥토크 임시저장완료");
     }
 
     @Transactional
@@ -53,4 +68,14 @@ public class FoodTalkService {
 
         return ResponseEntity.ok(id + " 번 게시글 수정완료");
     }
+
+    public ResponseEntity<?> getFoodTalk(Long id) {
+
+        FoodTalk foodTalk = foodTalkRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(id + " 번의 게시글을 찾을 수 없습니다."));
+
+        return ResponseEntity.ok(foodTalk);
+    }
+
+
 }

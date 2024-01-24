4c794ed68a8e052fd51bbc1bba307ccb5a757728
헥스/박해승 <2001phs92@gmail.com>
2024-01-25 07:11:01 +0900

    Feat: password encoding 적용

--- src/main/java/homeat/backend/domain/user/dto/MemberRequest.java
+++ src/main/java/homeat/backend/domain/user/dto/MemberRequest.java
@@ -1,12 +1,14 @@
 package homeat.backend.domain.user.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 public class MemberRequest {
 
     @Getter
     public static class JoinDto {
         String email;
+        @Setter
         String password;
         String nickname;
     }
--- src/main/java/homeat/backend/domain/user/service/MemberCommandService.java
+++ src/main/java/homeat/backend/domain/user/service/MemberCommandService.java
@@ -5,6 +5,7 @@ import homeat.backend.domain.user.dto.MemberRequest;
 import homeat.backend.domain.user.entity.Member;
 import homeat.backend.domain.user.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberCommandService {
 
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder encoder;
 
     @Transactional
     public Member joinMember(MemberRequest.JoinDto request) {
 
+        request.setPassword(encoder.encode(request.getPassword()));
         Member newMember = MemberConverter.toMember(request);
+
         return memberRepository.save(newMember);
     }
 }

3d11818fea7095c064c86e5bda20ecd47efe0217
박준규 <junju404@naver.com>
2024-01-24 16:09:38 +0900

    Feat : home Controller 생성

---
+++ src/main/java/homeat/backend/domain/home/controller/ManagementController.java
@@ -0,0 +1,23 @@
+package homeat.backend.domain.home.controller;
+
+import homeat.backend.domain.home.dto.ManagementRequestDTO;
+import homeat.backend.domain.home.service.ManagementService;
+import io.swagger.annotations.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/home")
+@RequiredArgsConstructor
+public class ManagementController {
+
+    private final ManagementService managementService;
+
+    /**
+     * 목표 금액 저장
+     */
+//    @PostMapping("/save")
+//    public ResponseEntity<?> saveExpense(@RequestBody ManagementRequestDTO dto) { return  managementService.saveExpense(dto); }
+
+}

9cca1df65e7586991d03fe97eb053f8de08d0ab3
experi-do <goodijdh@naver.com>
2024-01-25 18:41:28 +0900

    Remove: Refactor로 인한 제거

--- src/main/java/homeat/backend/domain/homeatreport/repository/BadgePageTxtRepository.java
+++
@@ -1,9 +0,0 @@
-package homeat.backend.domain.homeatreport.repository;
-
-import homeat.backend.domain.homeatreport.entity.BadgePage_txt;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BadgePageTxtRepository extends JpaRepository<BadgePage_txt, Long> {
-}

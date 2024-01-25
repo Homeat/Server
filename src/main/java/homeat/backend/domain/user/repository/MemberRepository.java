17f0d16b025731ec668768d0dde1070111768d9c
헥스/박해승 <2001phs92@gmail.com>
2024-01-26 01:13:31 +0900

    Feat: 이메일, 닉네임 중복 예외 처리

--- src/main/java/homeat/backend/domain/user/repository/MemberRepository.java
+++ src/main/java/homeat/backend/domain/user/repository/MemberRepository.java
@@ -2,6 +2,12 @@ package homeat.backend.domain.user.repository;
 
 import homeat.backend.domain.user.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByNickname(String nickname);
 }
--- src/main/java/homeat/backend/domain/user/service/MemberCommandService.java
+++ src/main/java/homeat/backend/domain/user/service/MemberCommandService.java
@@ -3,7 +3,10 @@ package homeat.backend.domain.user.service;
 import homeat.backend.domain.user.controller.MemberConverter;
 import homeat.backend.domain.user.dto.MemberRequest;
 import homeat.backend.domain.user.entity.Member;
+import homeat.backend.domain.user.handler.MemberErrorStatus;
+import homeat.backend.domain.user.handler.MemberHandler;
 import homeat.backend.domain.user.repository.MemberRepository;
+import homeat.backend.global.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,19 @@ public class MemberCommandService {
     @Transactional
     public Member joinMember(MemberRequest.JoinDto request) {
 
+        // 중복 이메일 처리
+        memberRepository.findByEmail(request.getEmail())
+                .ifPresent(member -> {
+                    throw new MemberHandler(MemberErrorStatus.EXIST_EMAIL);
+                });
+
+        // 중복 닉네임 처리
+        memberRepository.findByNickname(request.getNickname())
+                .ifPresent(member -> {
+                    throw new MemberHandler(MemberErrorStatus.EXIST_NICKNAME);
+                });
+
+        // 예외 없음
         request.setPassword(encoder.encode(request.getPassword()));
         Member newMember = MemberConverter.toMember(request);
 

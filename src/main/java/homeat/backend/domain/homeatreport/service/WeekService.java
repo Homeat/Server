76fa5d983b6b34cef556ba87936afdc5f58930fa
experi-do <goodijdh@naver.com>
2024-01-25 16:23:36 +0900

    feat: WeekService 생성

--- src/main/java/homeat/backend/domain/homeatreport/service/WeekService.java
+++ src/main/java/homeat/backend/domain/homeatreport/service/WeekService.java
@@ -1,4 +1,36 @@
 package homeat.backend.domain.homeatreport.service;
 
+import homeat.backend.domain.homeatreport.entity.Week;
+import homeat.backend.domain.homeatreport.entity.WeekStatus;
+import homeat.backend.domain.homeatreport.repository.WeekRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class WeekService {
+
+    @Autowired
+    private final WeekRepository weekRepository;
+
+    /**
+     * 이번주 목표식비 조회
+     */
+    public Long getGoalPrice(Long id) {
+        Week week = weekRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(id + " 번의 week를 찾을 수 없습니다."));
+        return week.getGoal_price();
+    }
+
+    /**
+     * 이번주 달성여부 조회
+     */
+    public WeekStatus getWeekStatus(Long id) {
+        Week week = weekRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(id + " 번의 week를 찾을 수 없습니다."));
+        return week.getWeek_status();
+    }
 }

f9fd1c4f3dba435695f3a27fe45d7a8460cad8b6
헥스/박해승 <2001phs92@gmail.com>
2024-01-26 01:12:52 +0900

    Feat: 에러 처리 handler 작성

---
+++ src/main/java/homeat/backend/domain/user/handler/MemberErrorStatus.java
@@ -0,0 +1,39 @@
+package homeat.backend.domain.user.handler;
+
+import homeat.backend.global.payload.BaseStatus;
+import homeat.backend.global.payload.ReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum MemberErrorStatus implements BaseStatus {
+
+    EXIST_EMAIL(HttpStatus.CONFLICT, "MEMBER_4090", "이미 존재하는 이메일입니다."),
+    EXIST_NICKNAME(HttpStatus.CONFLICT, "MEMBER_4091", "이미 존재하는 닉네임입니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDTO getReason() {
+        return ReasonDTO.builder()
+                .isSuccess(false)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ReasonDTO getReasonHttpStatus() {
+        return ReasonDTO.builder()
+                .isSuccess(false)
+                .code(code)
+                .message(message)
+                .httpStatus(httpStatus)
+                .build();
+    }
+}

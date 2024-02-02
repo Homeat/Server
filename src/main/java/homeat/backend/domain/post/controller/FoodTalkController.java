e3ea73a1917be6c73f37f9662889053f72560036
wooandrich <brad1182@naver.com>
2024-02-02 16:44:36 +0900

    etc : 주석 제거

--- src/main/java/homeat/backend/domain/post/controller/FoodTalkController.java
+++ src/main/java/homeat/backend/domain/post/controller/FoodTalkController.java
@@ -8,12 +8,10 @@ import java.util.List;
 import javax.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -117,14 +115,6 @@ public class FoodTalkController {
     public ResponseEntity<?> getFoodTalkByView(FoodTalkSearchCondition condition,@RequestParam Long id,@RequestParam int view) {
         return foodTalkService.getFoodTalkByView(condition,id,view);
     }
-
-
-
-
-
-    /**
-     * 검색
-     */
 
 
 

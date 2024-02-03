4f4c40d737cd1372f45c1648c24b9a2c0e480def
parkhs21 <2001phs92@gmail.com>
2024-02-04 06:10:45 +0900

    Docs: 스웨거 정보 추가

--- src/main/java/homeat/backend/domain/user/controller/MemberController.java
+++ src/main/java/homeat/backend/domain/user/controller/MemberController.java
@@ -7,6 +7,8 @@ import homeat.backend.domain.user.service.MemberCommandService;
 import homeat.backend.domain.user.service.MemberQueryService;
 import homeat.backend.global.payload.ApiPayload;
 import homeat.backend.global.payload.CommonSuccessStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
@@ -19,18 +21,21 @@ import java.util.Date;
 @RestController
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "Member", description = "회원 관련 api")
 @RequestMapping("/v1/members")
 public class MemberController {
 
     private final MemberCommandService memberCommandService;
     private final MemberQueryService memberQueryService;
 
+    @Operation(summary = "회원가입 api")
     @PostMapping("/join")
     public ApiPayload<MemberResponse.JoinResultDTO> create(@RequestBody @Valid MemberRequest.JoinDto request) {
         Member member = memberCommandService.joinMember(request);
         return ApiPayload.onSuccess(CommonSuccessStatus.CREATED, MemberConverter.toJoinResultDTO(member));
     }
 
+    @Operation(summary = "로그인 api")
     @PostMapping("/login")
     public ApiPayload<MemberResponse.LoginResultDTO> login(@RequestBody @Valid MemberRequest.LoginDto request) {
         String token = memberCommandService.loginMember(request);
@@ -38,6 +43,7 @@ public class MemberController {
         return ApiPayload.onSuccess(CommonSuccessStatus.OK, MemberConverter.toLoginResultDTO(token, expiredAt));
     }
 
+    @Operation(summary = "회원정보 api")
     @GetMapping("/mypage")
     public ApiPayload<MemberResponse.MyPageResultDTO> mypage(Authentication authentication) {
         Member member = memberQueryService.mypageMember(Long.parseLong(authentication.getName()));

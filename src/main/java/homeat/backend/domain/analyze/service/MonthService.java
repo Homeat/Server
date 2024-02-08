c1706dff7babe2ec98175377cdedaa5f514c15d9
박준규 <junju404@naver.com>
2024-02-09 03:09:30 +0900

    Feat : 매달 1일, 모든 유저 월 데이터 row 생성 로직 구현

---
+++ src/main/java/homeat/backend/domain/analyze/service/MonthService.java
@@ -0,0 +1,47 @@
+package homeat.backend.domain.analyze.service;
+
+import homeat.backend.domain.analyze.entity.FinanceData;
+import homeat.backend.domain.analyze.repository.FinanceDataRepository;
+import homeat.backend.domain.user.entity.Member;
+import homeat.backend.domain.user.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MonthService {
+
+    private final MemberRepository memberRepository;
+    private final FinanceDataRepository financeDataRepository;
+
+    /**
+     * 매달 1일, default row 생성
+     */
+
+    @Transactional
+    @Scheduled(cron = "0 0 0 1 * ?")
+    public void createMonthlyFinanceData() {
+        List<Member> members = memberRepository.findAll();
+        LocalDate today = LocalDate.now();
+
+        members.forEach(member -> {
+            Optional<FinanceData> existingFinanceData =
+                    financeDataRepository.findByMemberAndDate(member, today);
+
+            if (existingFinanceData.isEmpty()) {
+                FinanceData financeData = FinanceData.builder()
+                        .member(member)
+                        .build();
+
+                financeDataRepository.save(financeData);
+            }
+        });
+    }
+}

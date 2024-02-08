37aadb23f000223889c186099f9e3b6e7a0774cd
박준규 <junju404@naver.com>
2024-02-09 03:53:50 +0900

    Chore : 주석처리 된 query method

--- src/main/java/homeat/backend/domain/home/repository/DailyExpenseRepo.java
+++ src/main/java/homeat/backend/domain/home/repository/DailyExpenseRepo.java
@@ -1,14 +1,17 @@
 package homeat.backend.domain.home.repository;
 
 import homeat.backend.domain.home.entity.DailyExpense;
-import homeat.backend.domain.user.entity.Member;
+import homeat.backend.domain.home.repository.querydsl.DailyExpenseRepoCST;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DailyExpenseRepo extends JpaRepository<DailyExpense, Long>, DailyExpenseRepoCST {
     List<DailyExpense> findByMemberIdAndCreatedAtBetween(Long memberId, LocalDateTime localDateTime, LocalDateTime dateTime);
+    Optional<DailyExpense> findByMemberId(Long memberId);
 }

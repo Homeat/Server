65a61b311895849af9034aaf05887d89e2f7521d
박준규 <junju404@naver.com>
2024-02-09 03:11:20 +0900

    Feat : 해당 유저의 제일 최근 월 데이터 엔티티 조회

--- src/main/java/homeat/backend/domain/analyze/repository/FinanceDataRepository.java
+++ src/main/java/homeat/backend/domain/analyze/repository/FinanceDataRepository.java
@@ -1,9 +1,17 @@
 package homeat.backend.domain.analyze.repository;
 
 import homeat.backend.domain.analyze.entity.FinanceData;
+import homeat.backend.domain.analyze.repository.querydsl.FinanceRepositoryCustom;
+import homeat.backend.domain.user.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface FinanceDataRepository extends JpaRepository<FinanceData, Long> {
+public interface FinanceDataRepository extends JpaRepository<FinanceData, Long>, FinanceRepositoryCustom {
+    Optional<FinanceData> findTopByMemberIdOrderByCreatedAtDesc(Long memberId);
 }

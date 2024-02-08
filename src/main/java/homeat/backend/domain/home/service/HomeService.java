8718da033dcb03770fbd21e25e4a5ae3391dd6ce
박준규 <junju404@naver.com>
2024-02-09 03:56:58 +0900

    Feat : 홈 화면 조회 API 구현

--- src/main/java/homeat/backend/domain/home/service/HomeService.java
+++ src/main/java/homeat/backend/domain/home/service/HomeService.java
@@ -2,9 +2,12 @@ package homeat.backend.domain.home.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import homeat.backend.domain.home.converter.HomeConverter;
+import homeat.backend.domain.analyze.entity.FinanceData;
+import homeat.backend.domain.analyze.repository.FinanceDataRepository;
+import homeat.backend.domain.home.controller.HomeConverter;
 import homeat.backend.domain.home.dto.HomeRequestDTO;
 import homeat.backend.domain.home.dto.HomeResponseDTO;
+import homeat.backend.domain.home.entity.CostType;
 import homeat.backend.domain.home.entity.DailyExpense;
 import homeat.backend.domain.home.entity.Receipt;
 import homeat.backend.domain.home.repository.DailyExpenseRepo;
@@ -17,6 +20,7 @@ import homeat.backend.domain.user.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,10 +28,12 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @Transactional(readOnly = true)
@@ -37,6 +43,8 @@ public class HomeService {
     private final DailyExpenseRepo dailyExpenseRepo;
     private final ReceiptRepo receiptRepo;
     private final WeekRepository weekRepository;
+    private final FinanceDataRepository financeDataRepository;
+
     private final OCRService ocrService;
     private final FileUtil fileUtil;
 
@@ -56,9 +64,54 @@ public class HomeService {
         return ResponseEntity.ok("목표금액 저장완료");
     }
 
-    // 홈 화면 조회
-//    @Transactional
-//    public ResponseEntity<?> getHome(Long )
+    /**
+     * 홈 화면 조회
+     */
+    public ResponseEntity<HomeResponseDTO.HomeResultDTO> getHome(Member member) {
+        // 최근 월 데이터 조회
+        FinanceData financeData = financeDataRepository.findLatestFinanceDataIdByMember(member)
+                .orElseThrow(() -> new NoSuchElementException("해당 멤버는 월 데이터(finance)가 없습니다."));
+
+        // 목표 식비 조회
+        Long goalPrice = weekRepository.findTopByFinanceDataOrderByCreatedAtDesc(financeData)
+                .map(Week::getGoal_price)
+                .orElse(0L);
+
+        // 목표 식비가 0원 (default) -> nickname, 뱃지 개수만 반환
+        HomeResponseDTO.HomeResultDTO.HomeResultDTOBuilder builder = HomeResponseDTO.HomeResultDTO.builder()
+                .badgeCount(financeData.getNum_homeat_badge().intValue())
+                .nickname(member.getNickname());
+
+        // 목표 식비가 0원이 아닐 경우
+        if(goalPrice > 0) {
+            LocalDate today = LocalDate.now();
+            // 예외처리(저번 주 존재하지 않는 경우)
+            LocalDate lastMonday = today.minusWeeks(1).with(DayOfWeek.MONDAY);
+            LocalDate lastSunday = lastMonday.plusDays(6);
+            LocalDate thisMonday = today.with(DayOfWeek.MONDAY);
+
+            Long lastWeekTotal = dailyExpenseRepo.sumPricesBetweenDates(lastMonday, lastSunday);
+            Long thisWeekTotal = dailyExpenseRepo.sumPricesBetweenDates(thisMonday, today);
+
+            // 전주 대비 이번 주 절약 퍼센트
+            int thisWeekSavingPercent = (lastWeekTotal != null && thisWeekTotal != null && lastWeekTotal != 0) ? (int) ((double) (lastWeekTotal - thisWeekTotal) / lastWeekTotal * 100) : 0;
+            // 목표 금액에 대한 이번 주 남은 사용 퍼센트
+            int usedPercent = 100;
+            if (thisWeekTotal != null) {
+                usedPercent = (int) (usedPercent - (double) thisWeekTotal / goalPrice * 100);
+            }
+
+            // 목표 금액 & 전주 대비 이번 주 절약 퍼센트 & 사용 금액 & 목표 금액 대비 사용 금액 퍼센트
+            builder.targetMoney(goalPrice)
+                    .lastWeekSavingPercent(thisWeekSavingPercent)
+                    .usedMoney(thisWeekTotal)
+                    .usedPercent(usedPercent);
+        }
+
+        HomeResponseDTO.HomeResultDTO result = builder.build();
+
+        return ResponseEntity.ok(result);
+    }
 
     /**
      * OCR 영수증 처리

525e286f83f013504a4c1f4b3d42026c9f955274
박준규 <junju404@naver.com>
2024-02-09 03:08:16 +0900

    Refact : 비용들 default 설정 & Feat : 비용 추가 시, 누적 합 메소드 구현

--- src/main/java/homeat/backend/domain/analyze/entity/FinanceData.java
+++ src/main/java/homeat/backend/domain/analyze/entity/FinanceData.java
@@ -13,6 +13,7 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FinanceData extends BaseEntity {
 
+    // 회원가입 시 1회 + 매달 1일마다 1회 + (InActive -> Active 마다 1회 생성)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "finance_data_id")
@@ -22,12 +23,19 @@ public class FinanceData extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    private Long month_jipbap_price; // 이번달 집밥 비용
+    @Builder.Default
+    private Long month_jipbap_price = 0L; // 이번달 집밥 비용
 
-    private Long month_out_price; // 이번달 배달/외식 비용
-
-    private Long income; // 사용자 수입
+    @Builder.Default
+    private Long month_out_price = 0L; // 이번달 배달/외식 비용
 
     private Long num_homeat_badge; // 홈잇뱃지 개수
 
+    public void addJipbapPrice(long price) {
+        this.month_jipbap_price += price;
+    }
+
+    public void addOutPrice(long price) {
+        this.month_out_price += price;
+    }
 }

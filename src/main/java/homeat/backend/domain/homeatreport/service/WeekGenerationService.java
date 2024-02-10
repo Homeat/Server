9c40b26e0ea294de1537d139c4c6e07ef7a4c11a
experi-do <goodijdh@naver.com>
2024-02-10 23:08:06 +0900

    Feat: week 엔티티 생성 시점에 month가 바뀌었으면 financedata 엔티티 새로 생성(num_homeat_badge는 값을 넣어서 build)

--- src/main/java/homeat/backend/domain/homeatreport/service/WeekGenerationService.java
+++ src/main/java/homeat/backend/domain/homeatreport/service/WeekGenerationService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service

aa524ca2c2fe6ee0b201b657afdd503d2c3d6924
experi-do <goodijdh@naver.com>
2024-02-11 14:01:32 +0900

    Fix: ReportWeeklyResponseDTO 필드 수정

--- src/main/java/homeat/backend/domain/homeatreport/dto/ReportWeeklyResponseDTO.java
+++ src/main/java/homeat/backend/domain/homeatreport/dto/ReportWeeklyResponseDTO.java
@@ -1,5 +1,6 @@
 package homeat.backend.domain.homeatreport.dto;
 
+import homeat.backend.domain.user.entity.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,8 +10,13 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ReportWeeklyResponseDTO {
 
-    private String badge_img_url; // 뱃지 이미지 url
-    private Long exceed_price; // 이번주 초과 금액
-    private Long goal_price; // 이번주 목표 금액
+    private String age_range; // 연령대
+    private Long income; // 수입
+    private Gender gender; // 성별
+    private String nickname; // 닉네임
+    private Long jipbap_save; // 집밥 절약 비용
+    private Long out_save; // 외식/배달 절약 비용
+    private Double jipbap_average; // 비교군 집밥 평균 지출 금액
+    private Long month_jipbap_price; // 본인 집밥 지출 금액
 
 }

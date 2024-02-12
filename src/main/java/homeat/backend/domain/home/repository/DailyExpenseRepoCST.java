96bd0bdd97dbc0a9f9f6e518ab551a5d7271a155
experi-do <goodijdh@naver.com>
2024-02-13 05:07:16 +0900

    Feat: findDailyExpenseByMemberIdAndDateBetween 생성
    멤버id와 주 시작일/마지막일로 DailyExpense 찾기

--- src/main/java/homeat/backend/domain/home/repository/DailyExpenseRepoCST.java
+++ src/main/java/homeat/backend/domain/home/repository/DailyExpenseRepoCST.java
@@ -1,4 +1,12 @@
 package homeat.backend.domain.home.repository;
 
+import homeat.backend.domain.home.entity.DailyExpense;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface DailyExpenseRepoCST {
+
+    List<DailyExpense> findDailyExpenseByMemberIdAndDateBetween(Long member_id, LocalDate startOfWeek, LocalDate endOfWeek);
+
 }

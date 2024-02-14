e452e5338e78367b95ca9d7a48a5e389b729a2e2
experi-do <goodijdh@naver.com>
2024-02-15 02:41:24 +0900

    Chore: 미사용 import 삭제

--- src/main/java/homeat/backend/domain/homeatreport/controller/HomeatReportAnalyzeController.java
+++ src/main/java/homeat/backend/domain/homeatreport/controller/HomeatReportAnalyzeController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 

25599d7a7d678a793e8566833943bcc0afdc374b
experi-do <goodijdh@naver.com>
2024-02-19 05:45:19 +0900

    FIX: Schedule 시간 조정

--- src/main/java/homeat/backend/domain/homeatreport/service/WeekGenerationService.java
+++ src/main/java/homeat/backend/domain/homeatreport/service/WeekGenerationService.java
@@ -30,7 +30,7 @@ public class WeekGenerationService {
     private final BadgeImgRepository badgeImgRepository;
     private final MemberRepository memberRepository;
 
-    @Scheduled(cron = "0 38 04 * * MON")
+    @Scheduled(cron = "0 0 0 * * SUN")
     public void generateNewWeekMembers() {
         List<Member> members = memberRepository.findAll();
         System.out.println("members: "+members.size());

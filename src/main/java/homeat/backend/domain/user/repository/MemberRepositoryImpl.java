c72145c83a11febc3c437c9ca08c209229055bad
experi-do <goodijdh@naver.com>
2024-02-20 06:00:15 +0900

    Fix: findMemberByCriteria qeury 수정

--- src/main/java/homeat/backend/domain/user/repository/MemberRepositoryImpl.java
+++ src/main/java/homeat/backend/domain/user/repository/MemberRepositoryImpl.java
@@ -3,12 +3,14 @@ package homeat.backend.domain.user.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import homeat.backend.domain.user.entity.Gender;
 import homeat.backend.domain.user.entity.Member;
+import homeat.backend.domain.user.entity.QMember;
 import homeat.backend.domain.user.entity.QMemberInfo;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static homeat.backend.domain.user.entity.QMember.member;
 
@@ -19,19 +21,22 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
     public MemberRepositoryImpl(EntityManager em) { this.queryFactory = new JPAQueryFactory(em); }
 
     @Override
-    public List<Member> findMemberByCriteria(Integer[] ageRange, Gender gender, Long income) {
+    public Optional<List<Member>> findMemberByCriteria(Integer[] ageRange, Gender gender, Long income) {
         QMemberInfo qMemberInfo = QMemberInfo.memberInfo;
+        QMember qMember = QMember.member;
 
-        LocalDate startDate = LocalDate.now().minusYears(ageRange[1]);
-        LocalDate endDate = LocalDate.now().minusYears(ageRange[0]);
+        Integer startYear = ageRange[0];
+        Integer endYear = ageRange[1];
 
-        List<Member> members = queryFactory.selectFrom(member)
-                .innerJoin(qMemberInfo.member, member)
-                .where(qMemberInfo.birth.between(startDate, endDate)
+        List<Member> members =
+                queryFactory.selectFrom(qMember)
+                        .innerJoin(qMemberInfo).on(qMemberInfo.member.eq(qMember))
+                .where(qMemberInfo.birth.year()
+                        .between(startYear, endYear)
                         .and(qMemberInfo.gender.eq(gender))
                         .and(qMemberInfo.income.divide(100).eq(income/100)))
                 .fetch();
 
-        return members;
+        return Optional.ofNullable(members);
     }
 }

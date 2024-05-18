9fcb724983b853b7aaa26954c9fbb5538dd734eb
experi-do <goodijdh@naver.com>
2024-05-18 21:40:28 +0900

    [HOMEAT-32] Feat: 홍밋리포트 소비분석 관련 repository findFinanceDataById(financedata), findAllByCreatedAtBetween(weekanalyze), findTopByMemberOrderByIdDesc(weekanalyze) 추가

--- src/main/java/homeat/backend/domain/homeatreport/repository/WeekAnalyzeRepository.java
+++ src/main/java/homeat/backend/domain/homeatreport/repository/WeekAnalyzeRepository.java
@@ -1,9 +1,17 @@
 package homeat.backend.domain.homeatreport.repository;
 
+import homeat.backend.domain.analyze.entity.FinanceData;
 import homeat.backend.domain.homeatreport.entity.Week_Analyze;
+import homeat.backend.domain.user.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface WeekAnalyzeRepository extends JpaRepository<Week_Analyze, Long> {
+    List<Week_Analyze> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+    Optional<Week_Analyze> findTopByMemberOrderByIdDesc(Member member);
 }

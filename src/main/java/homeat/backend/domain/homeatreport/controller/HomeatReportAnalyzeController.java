9a77b03dd38bbab57f719b08193f868a6d332a59
experi-do <goodijdh@naver.com>
2024-05-18 21:41:23 +0900

    [HOMEAT-32] Feat: 홍밋리포트 소비분석 관련 HomeatReportAnalyzeController 메서드별 기본 파라미터 설정

--- src/main/java/homeat/backend/domain/homeatreport/controller/HomeatReportAnalyzeController.java
+++ src/main/java/homeat/backend/domain/homeatreport/controller/HomeatReportAnalyzeController.java
@@ -31,10 +31,12 @@ public class HomeatReportAnalyzeController {
      * @param authentication
      * @return
      */
+
     @Operation(summary = "홈잇리포트 소비분석 상단의 날짜 조회 api")
     @GetMapping("/ofMonth")
     public ApiPayload<ReportMonthlyAnalyzeResponseDTO> getMonthInput(
-            @RequestParam String input_year, String input_month,
+            @RequestParam(value = "input_year", defaultValue = "#{T(java.time.LocalDate).now().getYear()}") String input_year,
+            @RequestParam(value = "input_month", defaultValue = "#{T(java.time.LocalDate).now().getMonthValue()}") String input_month,
             Authentication authentication
     ) {
         Member member = memberQueryService.mypageMember(Long.parseLong(authentication.getName()));
@@ -53,10 +55,13 @@ public class HomeatReportAnalyzeController {
      * @param authentication
      * @return
      */
+
     @Operation(summary = "홈잇리포트 소비분석 하단의 날짜 조회 api")
     @GetMapping("/ofWeek")
     public ApiPayload<ReportWeeklyResponseDTO> getWeekInput(
-            @RequestParam String input_year, String input_month, String input_day,
+            @RequestParam(value = "input_year", defaultValue = "#{T(java.time.LocalDate).now().getYear()}") String input_year,
+            @RequestParam(value = "input_month", defaultValue = "#{T(java.time.LocalDate).now().getMonthValue()}") String input_month,
+            @RequestParam(value = "input_day", defaultValue = "#{T(java.time.LocalDate).now().getDayOfMonth()") String input_day,
             Authentication authentication
     ) {
         Member member = memberQueryService.mypageMember(Long.parseLong(authentication.getName()));

cc2375af4a8baf8fe81c2593b42da4e4bdf42e78
experi-do <goodijdh@naver.com>
2024-05-19 04:04:45 +0900

    [HOMEAT-32] Chore: 미사용 파일 주석 처리

--- src/main/java/homeat/backend/domain/homeatreport/repository/WeekRepository.java
+++ src/main/java/homeat/backend/domain/homeatreport/repository/WeekRepository.java
@@ -9,12 +9,12 @@ import java.time.LocalDateTime;
 
 @Repository
 public interface WeekRepository extends JpaRepository<Week, Long>, WeekRepositoryCustom {
-    Optional<Week> findFirstByFinanceDataOrderByCreatedAtDesc(FinanceData financeData);
-    Optional<Week> findFirstByFinanceDataAndCreatedAtBetween(FinanceData financeData, LocalDateTime start, LocalDateTime end);
+    //Optional<Week> findFirstByFinanceDataOrderByCreatedAtDesc(FinanceData financeData);
+    //Optional<Week> findFirstByFinanceDataAndCreatedAtBetween(FinanceData financeData, LocalDateTime start, LocalDateTime end);
 
     Optional<Week> findTopByFinanceDataOrderByIdDesc(FinanceData financeData);
 
     Optional<Week> findTopByFinanceDataOrderByFinanceDataIdDesc(FinanceData financeData);
-    List<Week> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+    //List<Week> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
 */

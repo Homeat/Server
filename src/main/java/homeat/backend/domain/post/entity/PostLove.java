2ae9ff26e75083a9820d5a470953c4dc2f5e7646
wooandrich <brad1182@naver.com>
2024-07-23 17:22:31 +0900

    HOMEAT-104 fix : 좋아요 테이블 유니크 제약조건 추가

--- src/main/java/homeat/backend/domain/post/entity/PostLove.java
+++ src/main/java/homeat/backend/domain/post/entity/PostLove.java
@@ -1,16 +1,9 @@
 package homeat.backend.domain.post.entity;
 
 import homeat.backend.domain.user.entity.Member;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+
+import javax.persistence.*;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,6 +15,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"member_id", "mappingId", "postType"}))
 public class PostLove {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
